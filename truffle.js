817394e4af893e97f2c846156e2cf832b68fbcda
mxpaul <mxpaul>
2018-07-06 14:29:40 +0300

    let "truffle test" support mocha --grep <regexp> option to only run specified test cases

--- truffle.js
+++ truffle.js
@@ -3,6 +3,26 @@ require('babel-register')({
   ignore: /node_modules\/(?!openzeppelin-solidity)/
 });
 require('babel-polyfill');
+
+// -------------------------------------------------------------------
+// Emulate mocha --grep option to run only matching tests
+let mochaConf = {}; // Passed as module.exports.mocha
+// -------------------------------------------------------------------
+
+for (let i = 0; i < process.argv.length; i++) {
+	const arg = process.argv[i];
+	if (arg != '-g' && arg != "--grep" ) continue;
+	if (++i >= process.argv.length) {
+		console.error(arg + " option requires argument");
+		process.exit(1);
+	};
+	const re = new RegExp(process.argv[i]);
+	mochaConf.grep = new RegExp(process.argv[i]);
+	console.error("RegExp: " + i + ": " + re);
+	break;
+}
+// -------------------------------------------------------------------
+
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
@@ -13,4 +33,6 @@ module.exports = {
       network_id: "*" // match any network
     },
   },
+	mocha: mochaConf,
 };
+
