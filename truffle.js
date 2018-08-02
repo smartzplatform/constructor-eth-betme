444b19a201688ec4e1efdf244f31b44aff31ff9a
mxpaul <mxpaul>
2018-08-02 16:59:34 +0300

    better formating + localopts support in truffle.js

--- truffle.js
+++ truffle.js
@@ -1,8 +1,17 @@
-//require('babel-register');
 require('babel-register')({
   ignore: /node_modules\/(?!openzeppelin-solidity)/
 });
 require('babel-polyfill');
+
+// -------------------------------------------------------------------
+// localopts lets developer to use personal network credentials 
+// with no need to modify and commit truffle.js
+let localopts; try {
+	localopts = require('./localopts.js');
+} catch (e) {
+	localopts = {rinkeby: {etheraddress: "0x0000000000000000000000000000000000000000"}};
+}
+// -------------------------------------------------------------------
 
 // -------------------------------------------------------------------
 // Emulate mocha --grep option to run only matching tests
@@ -21,15 +30,21 @@ for (let i = 0; i < process.argv.length; i++) {
 // -------------------------------------------------------------------
 
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
-  networks: {
-    development: {
-      host: "127.0.0.1",
-      port: 9545,
-      network_id: "*" // match any network
-    },
-  },
 	mocha: mochaConf,
+	// See <http://truffleframework.com/docs/advanced/configuration>
+	// to customize your Truffle configuration!
+	networks: {
+		development: {
+			host: "127.0.0.1",
+			port: 9545,
+			network_id: "*" // match any network
+		},
+		rinkeby: {
+			host: "localhost", // Connect to geth on the specified
+			port: 8545,
+			from: localopts.rinkeby.etheraddress,
+			network_id: 4,
+			gas: 4612388, // Gas limit used for deploys
+		}
+	},
 };
-
